1d90a1b33c83437a67ccfe436b36772250336971
Aji Setiawan <ajiisetiawan09@gmail.com>
2025-02-21 11:51:47 +0700

    coba upload di vercel

--- src/components/ShareLinkButton.jsx
+++ src/components/ShareLinkButton.jsx
@@ -1,12 +1,23 @@
 "use client"
 
+import { useState } from "react"
+import { LinkIcon } from "@heroicons/react/20/solid"
+
 export default function ShareLinkButton() {
+    const [copied, setCopied] = useState(false)
+
+
     function handleClick() {
-        console.log("Cliked")
+        navigator.clipboard.writeText(window.location.href)
+        setCopied(true)
+        setTimeout(() => setCopied(false), 1500)
     }
 
 
     return (
-        <button onClick={handleClick} className="border px-2 py-1 rounded text-gray-500 rounded text-sm hover:bg-gray-200 hover:text-gray-700 mb-3">Copy Link</button>
+        <button onClick={handleClick} className=" flex items-center gap-1 border px-2 py-1 rounded text-gray-500 rounded text-sm hover:bg-gray-200 hover:text-gray-700 mb-3">
+            <LinkIcon className="w-4 h-4 " />
+            {copied ? "Link Copied" : "Share Link"}
+        </button>
     )
 }
